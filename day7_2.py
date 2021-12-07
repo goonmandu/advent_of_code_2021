b989c7b763b4da6972bb4284cf335d456f1d1d79
goonmandu <61984863+goonmandu@users.noreply.github.com>
2021-12-07 01:45:09 -0500

    Create day7_2.py

---
+++ day7_2.py
@@ -0,0 +1,21 @@
+import math
+
+
+def main():
+    with open("AoC-D7-2.txt") as f:
+        crab_positions = f.readlines()[0].split(",")
+    crab_positions = [int(pos) for pos in crab_positions]
+    crab_positions.sort()
+    mean_1 = math.floor(sum(crab_positions) / len(crab_positions))
+    mean_2 = math.ceil(sum(crab_positions) / len(crab_positions))
+    total_fuel_1, total_fuel_2 = 0, 0
+    for pos in crab_positions:
+        for i in range(abs(mean_1 - pos)):
+            total_fuel_1 += i + 1
+        for i in range(abs(mean_2 - pos)):
+            total_fuel_2 += i + 1
+    print(f"Answer is either {total_fuel_1} or {total_fuel_2}.")
+
+
+if __name__ == "__main__":
+    main()
