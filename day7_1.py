31966cffa09cb0a1048214146d1dd5d3f10d5311
goonmandu <61984863+goonmandu@users.noreply.github.com>
2021-12-07 01:44:03 -0500

    Create day7_1.py

---
+++ day7_1.py
@@ -0,0 +1,14 @@
+import statistics
+
+
+def main():
+    with open("AoC-D7-2.txt") as f:
+        crab_positions = f.readlines()[0].split(",")
+    crab_positions = [int(i) for i in crab_positions]
+    crab_positions.sort()
+    median = statistics.median(crab_positions)
+    print(f"Minimum combined fuel is {sum([abs(int(median) - int(pos)) for pos in crab_positions])}.")
+
+
+if __name__ == "__main__":
+    main()
